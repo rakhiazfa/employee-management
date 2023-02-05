1e3159945de3bf0060eb44534e3cb7143d9be9ff
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-05 19:43:22 +0700

    Create profile page.

---
+++ views/profile.php
@@ -0,0 +1,26 @@
+<div class="navbar-bg"></div>
+
+<!-- Topbar -->
+
+<?php topbar() ?>
+
+<!-- Sidebar -->
+<?php sidebar() ?>
+
+<!-- Main Content -->
+<div class="main-content">
+
+    <section class="section">
+
+        <div class="row">
+
+            <div class="col-12">
+
+
+
+            </div>
+        </div>
+
+    </section>
+
+</div>
