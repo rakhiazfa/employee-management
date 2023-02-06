29f111b4b9df726ce026f1be35d27211744c6bbf
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-06 23:52:53 +0700

    Convert start and end time.

--- actions/shifts/store.php
+++ actions/shifts/store.php
@@ -6,6 +6,9 @@ $name = htmlspecialchars($_POST['name'] ?? null);
 $start = htmlspecialchars($_POST['start'] ?? null);
 $end = htmlspecialchars($_POST['end']);
 
+$start = date('H:i:s', strtotime($start));
+$end = date('H:i:s', strtotime($end));
+
 $query = $connection->execute_query("INSERT INTO shifts (name, start, end) VALUES (
     ?, ?, ?
 )", [$name, $start, $end]);
