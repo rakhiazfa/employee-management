049acf0a71a79ff2abfb2a58779a6148aeb2ac36
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-06 12:50:10 +0700

    Create 403 error page.

---
+++ views/403.php
@@ -0,0 +1,13 @@
+<div class="page-error">
+    <div class="page-inner">
+        <h1>403</h1>
+        <div class="page-description">
+            Anda tidak memiliki akses ke halaman ini.
+        </div>
+        <div class="page-search">
+            <div class="mt-3">
+                <a href="<?php echo env('APP_URL') ?>">Back to Home</a>
+            </div>
+        </div>
+    </div>
+</div>
