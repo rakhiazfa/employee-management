9b78c3b20258fc57b2c6b2c5c0747f952515eb53
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-06 13:10:07 +0700

    Slicing presence page - employee role.

---
+++ views/user/presences.php
@@ -0,0 +1,101 @@
+<?php
+
+global $connection;
+
+$iteration = 1;
+
+?>
+
+<div class="navbar-bg"></div>
+
+<!-- Topbar -->
+
+<?php topbar() ?>
+
+<!-- Sidebar -->
+<?php sidebar() ?>
+
+<!-- Main Content -->
+<div class="main-content">
+
+    <section class="section">
+
+        <div class="row">
+
+            <div class="col-12">
+
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h4>Formulir Kehadiran</h4>
+                    </div>
+                    <div class="card-body">
+
+
+
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+        <div class="row">
+
+            <div class="col-12">
+                <div class="card card-success">
+                    <div class="card-header">
+                        <h4>Riwayat Kehadiran</h4>
+                    </div>
+                    <div class="card-body">
+
+                        <?php if (hasFlash('success')) { ?>
+                            <div class="alert alert-success">
+                                <?php echo flash('success') ?>
+                            </div>
+                        <?php } ?>
+
+                        <div class="table-responsive">
+                            <table class="table-sm table-bordered">
+                                <thead>
+                                    <tr>
+                                        <th>No</th>
+                                        <th>Employee</th>
+                                        <th>Date</th>
+                                        <th>Presence Time</th>
+                                        <th>Late Time</th>
+                                        <th colspan="2">Shift</th>
+                                        <th>Status</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td rowspan="3">1</td>
+                                        <th rowspan="3">Rakhi Azfa Rifansya</th>
+                                        <td rowspan="3">20 July 2004</td>
+                                        <td rowspan="3">08:00</td>
+                                        <td rowspan="3">0 Minutes</td>
+                                        <td colspan="2" class="text-center">Shift Pagi</td>
+                                        <td rowspan="3">
+                                            <div class="badge badge-success">Present</div>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <th>Start</th>
+                                        <th>08:00</th>
+                                    </tr>
+                                    <tr>
+                                        <th>End</th>
+                                        <th>10:00</th>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </section>
+
+</div>
