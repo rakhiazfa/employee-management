02ad8f0d62f1aed25659866375de091fde02641f
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-06 12:55:25 +0700

    Add rendering conditions - sidebar.

--- views/components/auth/sidebar.php
+++ views/components/auth/sidebar.php
@@ -23,28 +23,32 @@
 
             <li class="menu-header">Menu / Item</li>
 
-            <li>
-                <a class="nav-link" href="<?php echo url('employees') ?>">
-                    <i class="fas fa-users"></i>
-                    <span>Daftar Karyawan</span>
-                </a>
-            </li>
+            <?php if ($_SESSION['user']['role'] === 'admin') { ?>
+                <li>
+                    <a class="nav-link" href="<?php echo url('employees') ?>">
+                        <i class="fas fa-users"></i>
+                        <span>Daftar Karyawan</span>
+                    </a>
+                </li>
 
-            <li>
-                <a class="nav-link" href="<?php echo url('presences') ?>">
-                    <i class="fas fa-calendar"></i>
-                    <span>Kehadiran Karyawan</span>
-                </a>
-            </li>
+                <li>
+                    <a class="nav-link" href="<?php echo url('presences') ?>">
+                        <i class="fas fa-calendar"></i>
+                        <span>Kehadiran Karyawan</span>
+                    </a>
+                </li>
+            <?php } ?>
 
             <li class="menu-header">Laporan</li>
 
-            <li>
-                <a class="nav-link" href="<?php echo url('reports') ?>">
-                    <i class="fas fa-file-signature"></i>
-                    <span>Laporan Kehadiran</span>
-                </a>
-            </li>
+            <?php if ($_SESSION['user']['role'] === 'admin') { ?>
+                <li>
+                    <a class="nav-link" href="<?php echo url('reports') ?>">
+                        <i class="fas fa-file-signature"></i>
+                        <span>Laporan Kehadiran</span>
+                    </a>
+                </li>
+            <?php } ?>
 
         </ul>
 
