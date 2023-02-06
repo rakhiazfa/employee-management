b4735b3bc4dad8bcba2e5adc6cbfd928eeaab17b
Rakhi Azfa Rifansya <rakhiazfa0421@gmail.com>
2023-02-06 11:50:26 +0700

    Update favicon.

--- views/components/layout.php
+++ views/components/layout.php
@@ -6,6 +6,8 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
+    <link rel="shortcut icon" href="<?php echo asset('img/employee-management.png') ?>" type="image/x-icon">
+
     <!-- General CSS Files -->
     <link rel="stylesheet" href="<?php echo asset('modules/bootstrap/css/bootstrap.min.css') ?>">
     <link rel="stylesheet" href="<?php echo asset('modules/fontawesome/css/all.min.css') ?>">
