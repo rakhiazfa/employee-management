fc79d9d5ee57a3e393177d8d3266e1a56fd6f62d
Fauzan Achmad <nmeylani49@gmail.com>
2023-02-09 20:05:04 +0700

    kelupaan

--- actions/paid_leaves/store.php
+++ actions/paid_leaves/store.php
@@ -39,42 +39,23 @@ $e = new DateTime($end);
 $total = $s->diff($e)->days + 1;
 
 
-$paidStart = DateTime::createFromFormat('H:i:s', $employee['start'] ?? '');
-$paidEnd = DateTime::createFromFormat('H:i:s', $employee['end'] ?? '');
-
 /**
  * Membuat kehadiran.
  * 
  */
 
 $query = $connection->execute_query("INSERT INTO paid_leaves 
-(start, end, total, description, status, employee_id) 
+(start, end, total, description) 
 VALUES (
-    ?, ?, ?, ?, ?, ?
-)", [$start, $end, $total, $description, '', $employeeId]);
+    ?, ?, ?, ?
+)", [$start, $end, $total, $description]);
 
-/**
- * Membuat riwayat cuti.
- * 
- */
 
-$query = $connection->execute_query("INSERT INTO paid_leave_histories 
-(employee_nip, employee_name, employee_email, shift_name, shift_start, shift_end, paid_leave_id) 
-VALUES (
-    ?, ?, ?, ?, ?, ?, ?
-)", [
-    $employee['nip'],
-    $employee['name'],
-    $employee['email'],
-    $start,
-    $end,
-    $connection->insert_id,
-]);
 
 $_SESSION['FLASH_MESSAGE']['success'] = [
     'value' => 'Berhasil mengirim permohonan.',
     'called' => false,
 ];
 
-header('Location: ' . env('APP_URL') . '/user/presences');
+header('Location: ' . env('APP_URL') . '/user/paid_leaves');
 die();
